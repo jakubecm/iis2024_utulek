c9f239215508dbfaf58b2f61430de82f907bd0be
Milan Jakubec <jakubeclp@gmail.com>
2024-11-20 18:04:50 +0100

    feat: users dashboard improvement part 2 - conditional render on tables

--- frontend/utulek/src/admin/UserTable.tsx
+++ frontend/utulek/src/admin/UserTable.tsx
@@ -1,8 +1,6 @@
 import { Button, Card, CardBody, CardFooter, Dialog, Typography } from "@material-tailwind/react";
 import { useState } from "react";
 import EditUserForm from "./EditUserForm";
- 
-const TABLE_HEAD = ["Username", "Full Name", "Email", "Role", "Telephone", "Specialization", "Verified", ""];
 
 const Roles: { [key: number]: string } = {
   0: "Admin",
@@ -25,10 +23,13 @@ export interface User {
 
 interface UserTableProps {
   users: User[];
+  role: string;
   onUpdateUser: () => void; // Function to handle updates
 }
 
-const UserTable: React.FC<UserTableProps> = ({ users, onUpdateUser }) => {
+const UserTable: React.FC<UserTableProps> = ({ users, role, onUpdateUser }) => {
+  type RoleType = keyof typeof columnConfig;
+  
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isEditOpen, setIsEditOpen] = useState(false);
 
@@ -42,101 +43,112 @@ const UserTable: React.FC<UserTableProps> = ({ users, onUpdateUser }) => {
     setIsEditOpen(false);
   };
 
+  const columnConfig = {
+    admin: ["Username", "Full Name", "Email", "Role", ""],
+    veterinarian: ["Username", "Full Name", "Email", "Role", "Specialization", "Telephone", ""],
+    caregiver: ["Username", "Full Name", "Email", "Role", ""],
+    volunteer: ["Username", "Full Name", "Email", "Verified", ""]
+  };
+  
+  const displayColumns = columnConfig[role as RoleType];
+
   return (
-    <Card className="h-full w-full overflow-scroll">
+    <Card className="h-full w-full">
       <table className="w-full min-w-max table-auto text-left">
         <thead>
           <tr>
-            {TABLE_HEAD.map((head) => (
-              <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
-                >
-                  {head}
+            {displayColumns.map((col) => (
+              <th key={col} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
+                  {col}
                 </Typography>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => {
-            const isLast = index === users.length - 1;
-            const classes = isLast ? "p-2" : "p-2 border-b border-blue-gray-50";
-            const fullName = `${user.FirstName} ${user.LastName}`;
-
-            return (
-              <tr key={user.Username}>
-                <td className={`${classes} bg-blue-gray-50/50`}>
+          {users.map((user) => (
+            <tr key={user.Id}>
+              {displayColumns.includes("Username") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
                     {user.Username}
                   </Typography>
                 </td>
-                <td className={classes}>
+              )}
+              {displayColumns.includes("Full Name") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
-                    {fullName}
+                    {`${user.FirstName} ${user.LastName}`}
                   </Typography>
                 </td>
-                <td className={`${classes} bg-blue-gray-50/50`}>
+              )}
+              {displayColumns.includes("Email") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
                     {user.Email}
                   </Typography>
                 </td>
-                <td className={classes}>
+              )}
+              {displayColumns.includes("Role") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
                     {Roles[user.role]}
                   </Typography>
                 </td>
-                <td className={`${classes} bg-blue-gray-50/50`}>
+              )}
+              {displayColumns.includes("Telephone") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
                     {user.Telephone || "-"}
                   </Typography>
                 </td>
-                <td className={classes}>
+              )}
+              {displayColumns.includes("Specialization") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
                     {user.Specialization || "-"}
                   </Typography>
                 </td>
-                <td className={`${classes} bg-blue-gray-50/50`}>
+              )}
+              {displayColumns.includes("Verified") && (
+                <td className="p-2 border-b border-blue-gray-50">
                   <Typography variant="small" color="blue-gray" className="font-normal">
-                    {user.verified && (user.verified ? "Yes" : "No") || "-" }
+                    {user.verified ? "Yes" : "No"}
                   </Typography>
                 </td>
-                <td className={classes}>
-                <Button color="blue" onClick={() => openEditModal(user)}>
-                    Edit
-                  </Button>
+              )}
+                <td className="p-2 border-b border-blue-gray-50 text-right">
+                <Button color="blue" onClick={() => openEditModal(user)}>Edit</Button>
                 </td>
-              </tr>
-            );
-          })}
+            </tr>
+          ))}
         </tbody>
       </table>
 
-      {/* Edit Modal */}
       {selectedUser && (
-      <Dialog open={isEditOpen} handler={closeEditModal} size="xs" className="!max-w-[30rem] !min-w-[26rem]">
-      <Card className="mx-auto w-full !max-w-[30rem] !min-w-[26rem] px-2">
-        <CardBody className="flex flex-col gap-4">
-            <EditUserForm
-              user={selectedUser}
-              onModification={() => {
-                closeEditModal();
-                onUpdateUser();
-              }}
-            />
-          </CardBody>
-          <CardFooter>
-            <Button variant="text" color="red" onClick={closeEditModal} className="mr-2">
-              Cancel
-            </Button>
-          </CardFooter>
+        <Dialog open={isEditOpen} handler={closeEditModal} size="xs" className="!max-w-[30rem] !min-w-[26rem]">
+          <Card className="mx-auto w-full !max-w-[30rem] !min-w-[26rem] px-2">
+            <CardBody className="flex flex-col gap-4">
+              <EditUserForm
+                user={selectedUser}
+                onModification={() => {
+                  closeEditModal();
+                  onUpdateUser();
+                }}
+              />
+            </CardBody>
+            <CardFooter>
+              <Button variant="text" color="red" onClick={closeEditModal} className="mr-2">
+                Cancel
+              </Button>
+            </CardFooter>
           </Card>
         </Dialog>
       )}
     </Card>
   );
-}
+};
 
 export default UserTable;
+
