18db1e634c118ccf33e63ee54e0a6b3b1aab84cb
Jakub Ráček <kubik54321@gmail.com>
2024-11-24 14:44:24 +0100

    feat: add short intro text

--- frontend/utulek/src/common/home.tsx
+++ frontend/utulek/src/common/home.tsx
@@ -110,112 +110,126 @@ const CatList: React.FC = () => {
     if (isDefaultImageAvailable) {
       return `${API_URL}/catphotos/default-image.png`;
     }
-    
+
     return ""; // No fallback image
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 py-6">
-      <Typography variant="h3" color="blue-gray" className="mb-8 text-center">
-        Available Cats
-      </Typography>
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-        {cats.map((cat) => {
-          const imageUrl = getImageUrl(cat);
-          return (
-            <Card key={cat.id} className="w-full max-w-md mx-auto shadow-lg">
-              <div className="relative">
-                {imageUrl ? (
-                  <img
-                    src={imageUrl}
-                    alt={cat.name}
-                    className="h-64 w-full object-cover rounded-t-lg"
-                    onError={(e) => {
-                      e.currentTarget.style.display = "none"; // Hide broken image
-                    }}
-                  />
-                ) : (
-                  <Typography color="gray" className="text-center py-10">
-                    No image available
-                  </Typography>
-                )}
-                {cat.photos && cat.photos.length > 1 && (
-                  <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between px-2">
-                    <Button color="blue-gray" size="sm" onClick={() => handlePreviousPhoto(cat.id)}>
-                      &lt;
-                    </Button>
-                    <Button color="blue-gray" size="sm" onClick={() => handleNextPhoto(cat.id)}>
-                      &gt;
-                    </Button>
-                  </div>
-                )}
-              </div>
-              <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
-                  {cat.name}
-                </Typography>
-                <Typography color="gray" className="text-sm mb-2">
-                  Age: {cat.age}
-                </Typography>
-                <Typography color="gray" className="text-sm mb-2">
-                  Species: {species[cat.species_id] || "Unknown"}
-                </Typography>
-                <Typography color="gray" className="text-sm">{cat.description}</Typography>
-              </CardBody>
-              <CardFooter className="flex justify-between">
-                {(role === Role.CAREGIVER || role === Role.VETS || role === Role.ADMIN) && (
-                  <Button
-                    color="blue-gray"
-                    size="sm"
-                    onClick={() => navigateToHealthRecords(cat.id)}
-                    className="mr-3"
-                  >
-                    <ListIcon fontSize="small" /> Health Records
-                  </Button>
-                )}
-                {(role === Role.CAREGIVER || role === Role.ADMIN) && (
-                  <>
-                    <Button color="blue-gray" className="mr-3" onClick={() => setSelectedCat(cat)}>
-                      Edit
-                    </Button>
-                    <DeleteCatButton catId={cat.id} onCatDeleted={handleCatDeleted} />
-                  </>
-                )}
-              </CardFooter>
-            </Card>
-          );
-        })}
+    <>
+      <div className="flex flex-col items-center my-8">
+        <Typography variant="h1" className="text-center mb-8">
+          Welcome to Rizztulek – A Home for Cats in Need
+        </Typography>
+        <Typography variant="lead" className="max-w-[70%]">
+          At Rizztulek, we are dedicated to providing love, care, and second chances to cats in need. Whether you're here to adopt a furry friend, learn about our shelter, or support our mission, you're in the right place.
+          <br /><br />
+          Explore our site to meet our adorable residents, and discover ways to get involved. Together, we can create a brighter future for every whiskered soul.
+          <br /><br />
+          Thank you for visiting Rizztulek – where every cat's story deserves a happy ending!
+        </Typography>
       </div>
-      {(role === Role.CAREGIVER || role === Role.ADMIN) && (
-        <Button color="blue" onClick={toggleModal} className="mt-8">
-          Add New Cat
-        </Button>
-      )}
-      <Dialog open={isModalOpen} handler={toggleModal} size="xs" className="!max-w-[30rem] !min-w-[26rem]">
-        <Card className="mx-auto w-full !max-w-[30rem] !min-w-[26rem] px-2">
-          <CardBody className="flex flex-col gap-4">
-            <AddCatForm onCatAdded={handleCatAdded} />
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button variant="text" color="red" onClick={toggleModal} className="mr-2 ml-a">
-              Cancel
-            </Button>
-          </CardFooter>
-        </Card>
-      </Dialog>
-      {selectedCat && (
-        <Dialog open={Boolean(selectedCat)} handler={() => setSelectedCat(null)} size="lg">
-          <DialogBody>
-            <EditCatForm cat={selectedCat} onCatUpdated={handleCatUpdated} onClose={() => setSelectedCat(null)} />
-          </DialogBody>
-          <DialogFooter>
-            <Button variant="text" color="red" onClick={() => setSelectedCat(null)} className="mr-2">
-              Cancel
-            </Button>
-          </DialogFooter>
+      <div className="flex flex-col items-center min-h-screen bg-gray-100 py-6">
+        <Typography variant="h3" color="blue-gray" className="mb-8 text-center">
+          Available Cats
+        </Typography>
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+          {cats.map((cat) => {
+            const imageUrl = getImageUrl(cat);
+            return (
+              <Card key={cat.id} className="w-full max-w-md mx-auto shadow-lg">
+                <div className="relative">
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt={cat.name}
+                      className="h-64 w-full object-cover rounded-t-lg"
+                      onError={(e) => {
+                        e.currentTarget.style.display = "none"; // Hide broken image
+                      }}
+                    />
+                  ) : (
+                    <Typography color="gray" className="text-center py-10">
+                      No image available
+                    </Typography>
+                  )}
+                  {cat.photos && cat.photos.length > 1 && (
+                    <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between px-2">
+                      <Button color="blue-gray" size="sm" onClick={() => handlePreviousPhoto(cat.id)}>
+                        &lt;
+                      </Button>
+                      <Button color="blue-gray" size="sm" onClick={() => handleNextPhoto(cat.id)}>
+                        &gt;
+                      </Button>
+                    </div>
+                  )}
+                </div>
+                <CardBody>
+                  <Typography variant="h5" color="blue-gray" className="mb-2 font-semibold">
+                    {cat.name}
+                  </Typography>
+                  <Typography color="gray" className="text-sm mb-2">
+                    Age: {cat.age}
+                  </Typography>
+                  <Typography color="gray" className="text-sm mb-2">
+                    Species: {species[cat.species_id] || "Unknown"}
+                  </Typography>
+                  <Typography color="gray" className="text-sm">{cat.description}</Typography>
+                </CardBody>
+                <CardFooter className="flex justify-between">
+                  {(role === Role.CAREGIVER || role === Role.VETS || role === Role.ADMIN) && (
+                    <Button
+                      color="blue-gray"
+                      size="sm"
+                      onClick={() => navigateToHealthRecords(cat.id)}
+                      className="mr-3"
+                    >
+                      <ListIcon fontSize="small" /> Health Records
+                    </Button>
+                  )}
+                  {(role === Role.CAREGIVER || role === Role.ADMIN) && (
+                    <>
+                      <Button color="blue-gray" className="mr-3" onClick={() => setSelectedCat(cat)}>
+                        Edit
+                      </Button>
+                      <DeleteCatButton catId={cat.id} onCatDeleted={handleCatDeleted} />
+                    </>
+                  )}
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </div>
+        {(role === Role.CAREGIVER || role === Role.ADMIN) && (
+          <Button color="blue" onClick={toggleModal} className="mt-8">
+            Add New Cat
+          </Button>
+        )}
+        <Dialog open={isModalOpen} handler={toggleModal} size="xs" className="!max-w-[30rem] !min-w-[26rem]">
+          <Card className="mx-auto w-full !max-w-[30rem] !min-w-[26rem] px-2">
+            <CardBody className="flex flex-col gap-4">
+              <AddCatForm onCatAdded={handleCatAdded} />
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button variant="text" color="red" onClick={toggleModal} className="mr-2 ml-a">
+                Cancel
+              </Button>
+            </CardFooter>
+          </Card>
         </Dialog>
-      )}
-    </div>
+        {selectedCat && (
+          <Dialog open={Boolean(selectedCat)} handler={() => setSelectedCat(null)} size="lg">
+            <DialogBody>
+              <EditCatForm cat={selectedCat} onCatUpdated={handleCatUpdated} onClose={() => setSelectedCat(null)} />
+            </DialogBody>
+            <DialogFooter>
+              <Button variant="text" color="red" onClick={() => setSelectedCat(null)} className="mr-2">
+                Cancel
+              </Button>
+            </DialogFooter>
+          </Dialog>
+        )}
+      </div>
+    </>
   );
 };
 
